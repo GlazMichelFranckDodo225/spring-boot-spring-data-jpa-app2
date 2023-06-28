3190d5261327fde96dab1128dc3d944ef9c3573c
FranckDodo91 <devlogappdgmf@gmail.com>
2023-06-28 05:36:12 +0200

    Creation of the "Employee" entity

---
+++ src/main/java/com/dgmf/spbtsdjpaapp2/entity/Employee.java
@@ -0,0 +1,19 @@
+package com.dgmf.spbtsdjpaapp2.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class Employee {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
