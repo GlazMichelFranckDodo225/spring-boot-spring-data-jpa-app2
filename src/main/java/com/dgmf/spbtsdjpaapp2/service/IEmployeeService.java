6b4d5baddb20338e7aa59fc03e6a66d299512d73
FranckDodo91 <devlogappdgmf@gmail.com>
2023-06-29 06:49:25 +0200

    Setting up the "Service" and "Controller" layers

---
+++ src/main/java/com/dgmf/spbtsdjpaapp2/service/IEmployeeService.java
@@ -0,0 +1,4 @@
+package com.dgmf.spbtsdjpaapp2.service;
+
+public interface IEmployeeService {
+}
---
+++ src/main/java/com/dgmf/spbtsdjpaapp2/service/impl/IEmployeeServiceImpl.java
@@ -0,0 +1,8 @@
+package com.dgmf.spbtsdjpaapp2.service.impl;
+
+import com.dgmf.spbtsdjpaapp2.service.IEmployeeService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class IEmployeeServiceImpl implements IEmployeeService {
+}
